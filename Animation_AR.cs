d6bffef10bf357df854a0f1c05156b7efb66f72d
Brian <brianhungerman@gmail.com>
2018-01-21 05:52:07 -0800

    Straw,Black,Rasp demos

--- Animation_AR.cs
+++ Animation_AR.cs
@@ -17,27 +17,9 @@ public class Animation_AR : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        sphere_speed *= .95f;
-
-        if (sphere_clicked != "")
-        {
-
-            //GameObject.Find(sphere_clicked).gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-           // GameObject.Find(sphere_clicked).gameObject.transform.localScale = new Vector3(GameObject.Find(sphere_clicked).gameObject.transform.localScale.x + .25f, GameObject.Find(sphere_clicked).gameObject.transform.localScale.y + .25f, GameObject.Find(sphere_clicked).gameObject.transform.localScale.y + .25f);
-
-        }
-
-        if (sphere_clicked == "")
-        {
             this.transform.Rotate(new Vector3(0, sphere_speed + .5f, 0));
-            for (int i = 0; i < 3; i++)
-            {
-                //this.transform.GetChild(i).rotation = Quaternion.Euler( new Vector3(0, 0, 0));//Rotate(new Vector3(Random.value, Random.value, Random.value));
-                
-            }
-
             this.transform.position = new Vector3(0, Mathf.Sin(Time.time * 2f) / 4f, 0);
-        }
+        
 
         if (Input.GetMouseButtonDown(0))
         { // if left button pressed...
@@ -51,21 +33,11 @@ public class Animation_AR : MonoBehaviour {
                     // do whatever you want
                     sphere_clicked = hit.collider.gameObject.name;
                     GameObject.Find("Canvas").GetComponent<UI_Base>().screen = sphere_clicked;
+                    GameObject.Find("Canvas").GetComponent<UI_Base>().berry = hit.collider.gameObject.transform.parent.parent.name ;
 
                 }
             }
             else sphere_clicked = "";
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                if (this.transform.GetChild(i).gameObject.name != sphere_clicked)
-                {
-                   // this.transform.GetChild(i).localScale = new Vector3(1, 1, 1);
-                }
-            }
         }
     }
 }  
--- UI_Base.cs
+++ UI_Base.cs
@@ -7,12 +7,19 @@ public class UI_Base : MonoBehaviour {
 
   
 
-    public GameObject FarmViewScreen;
-    public GameObject RecipeScreen;
+    public GameObject FarmViewScreenRasp;
+    public GameObject FarmViewScreenBlack;
+    public GameObject FarmViewScreenStraw;
+
+    public GameObject RecipeScreenRasp;
+    public GameObject RecipeScreenBlack;
+    public GameObject RecipeScreenStraw;
+
     public GameObject Recipe;
     public GameObject InfoScreen;
 
     public string screen;
+    public string berry;
 
     float timer = 0;
     // Use this for initialization
@@ -36,61 +43,187 @@ public class UI_Base : MonoBehaviour {
     void Update()
     {
         print(screen);
-        switch (screen)
+        switch (berry)
         {
-            case "Farm Viewer":
-                FarmViewScreen.SetActive(true);
-                FarmViewScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, FarmViewScreen.GetComponent<RectTransform>().localPosition.y * .7f, 0);
-                FarmViewScreen.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreen.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
-                if (FarmViewScreen.GetComponent<Image>().color.a > 1)
+            case "ImageTargetBlack":
+                switch (screen)
                 {
-                    FarmViewScreen.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-                }
-                break;
-            case "Info":
-                InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, InfoScreen.GetComponent<RectTransform>().localPosition.y * .7f, 0);
-                InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
-                if (InfoScreen.GetComponent<Image>().color.a > 1)
-                {
-                    InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-                }
-                print(InfoScreen.GetComponent<Image>().color.a);
-                break;
-            case "Recipes":
-                RecipeScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, RecipeScreen.GetComponent<RectTransform>().localPosition.y * .7f, 0);
-                RecipeScreen.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreen.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
-                if (RecipeScreen.GetComponent<Image>().color.a > 1)
-                {
-                    RecipeScreen.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-                }
-                break;
-            case "":
-               FarmViewScreen.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreen.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
-                if (FarmViewScreen.GetComponent<Image>().color.a < 0)
-                {
-                    FarmViewScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+                    case "Farm Viewer":
+                        FarmViewScreenBlack.SetActive(true);
+                        FarmViewScreenBlack.GetComponent<RectTransform>().localPosition = new Vector3(0, FarmViewScreenBlack.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        FarmViewScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreenBlack.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (FarmViewScreenBlack.GetComponent<Image>().color.a > 1)
+                        {
+                            FarmViewScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        break;
+                    case "Info":
+                        InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, InfoScreen.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (InfoScreen.GetComponent<Image>().color.a > 1)
+                        {
+                            InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        print(InfoScreen.GetComponent<Image>().color.a);
+                        break;
+                    case "Recipes":
+                        RecipeScreenBlack.GetComponent<RectTransform>().localPosition = new Vector3(0, RecipeScreenBlack.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        RecipeScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreenBlack.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (RecipeScreenBlack.GetComponent<Image>().color.a > 1)
+                        {
+                            RecipeScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        break;
+                    case "":
+                        FarmViewScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreenBlack.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (FarmViewScreenBlack.GetComponent<Image>().color.a < 0)
+                        {
+                            FarmViewScreenBlack.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
 
-                    FarmViewScreen.GetComponent<Image>().color = new Color(1, 1, 1, 0);
-                    FarmViewScreen.SetActive(false);
-                }
-                RecipeScreen.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreen.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
-                if (RecipeScreen.GetComponent<Image>().color.a < 0)
-                {
-                    RecipeScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+                            FarmViewScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                            FarmViewScreenBlack.SetActive(false);
+                        }
+                        RecipeScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreenBlack.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (RecipeScreenBlack.GetComponent<Image>().color.a < 0)
+                        {
+                            RecipeScreenBlack.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
 
-                    RecipeScreen.GetComponent<Image>().color = new Color(1, 1, 1, 0);
-                }
-                InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
-                if (InfoScreen.GetComponent<Image>().color.a < 0)
-                {
-                    InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+                            RecipeScreenBlack.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                        }
+                        InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (InfoScreen.GetComponent<Image>().color.a < 0)
+                        {
+                            InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
 
-                    InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                            InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                        }
+                        break;
+
+
                 }
                 break;
+            case "ImageTargetRasp":
+                switch (screen)
+                {
+                    case "Farm Viewer":
+                        FarmViewScreenRasp.SetActive(true);
+                        FarmViewScreenRasp.GetComponent<RectTransform>().localPosition = new Vector3(0, FarmViewScreenRasp.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        FarmViewScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreenRasp.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (FarmViewScreenRasp.GetComponent<Image>().color.a > 1)
+                        {
+                            FarmViewScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        break;
+                    case "Info":
+                        InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, InfoScreen.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (InfoScreen.GetComponent<Image>().color.a > 1)
+                        {
+                            InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        print(InfoScreen.GetComponent<Image>().color.a);
+                        break;
+                    case "Recipes":
+                        RecipeScreenRasp.GetComponent<RectTransform>().localPosition = new Vector3(0, RecipeScreenRasp.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        RecipeScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreenRasp.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (RecipeScreenRasp.GetComponent<Image>().color.a > 1)
+                        {
+                            RecipeScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        break;
+                    case "":
+                        FarmViewScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreenRasp.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (FarmViewScreenRasp.GetComponent<Image>().color.a < 0)
+                        {
+                            FarmViewScreenRasp.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+
+                            FarmViewScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                            FarmViewScreenRasp.SetActive(false);
+                        }
+                        RecipeScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreenRasp.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (RecipeScreenRasp.GetComponent<Image>().color.a < 0)
+                        {
+                            RecipeScreenRasp.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+
+                            RecipeScreenRasp.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                        }
+                        InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (InfoScreen.GetComponent<Image>().color.a < 0)
+                        {
+                            InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+
+                            InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                        }
+                        break;
+
+
+                }
+                break;
+
+            case "ImageTargetStraw":
+                switch (screen)
+                {
+                    case "Farm Viewer":
+                        FarmViewScreenStraw.SetActive(true);
+                        FarmViewScreenStraw.GetComponent<RectTransform>().localPosition = new Vector3(0, FarmViewScreenStraw.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        FarmViewScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreenStraw.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (FarmViewScreenStraw.GetComponent<Image>().color.a > 1)
+                        {
+                            FarmViewScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        break;
+                    case "Info":
+                        InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, InfoScreen.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (InfoScreen.GetComponent<Image>().color.a > 1)
+                        {
+                            InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        print(InfoScreen.GetComponent<Image>().color.a);
+                        break;
+                    case "Recipes":
+                        RecipeScreenStraw.GetComponent<RectTransform>().localPosition = new Vector3(0, RecipeScreenStraw.GetComponent<RectTransform>().localPosition.y * .7f, 0);
+                        RecipeScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreenStraw.GetComponent<Image>().color.a + Time.deltaTime * 2.5f);
+                        if (RecipeScreenStraw.GetComponent<Image>().color.a > 1)
+                        {
+                            RecipeScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                        break;
+                    case "":
+                        FarmViewScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, FarmViewScreenStraw.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (FarmViewScreenStraw.GetComponent<Image>().color.a < 0)
+                        {
+                            FarmViewScreenStraw.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+
+                            FarmViewScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                            FarmViewScreenStraw.SetActive(false);
+                        }
+                        RecipeScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, RecipeScreenStraw.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (RecipeScreenStraw.GetComponent<Image>().color.a < 0)
+                        {
+                            RecipeScreenStraw.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+
+                            RecipeScreenStraw.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                        }
+                        InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, InfoScreen.GetComponent<Image>().color.a - Time.deltaTime * 2.5f);
+                        if (InfoScreen.GetComponent<Image>().color.a < 0)
+                        {
+                            InfoScreen.GetComponent<RectTransform>().localPosition = new Vector3(0, -1920, 0);
+
+                            InfoScreen.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                        }
+                        break;
+
+
+                }
+                break;
+
+
+
 
 
         }
+        
 
      
 
